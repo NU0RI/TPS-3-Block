768a58b70dce164032d1900a31275f2ffff49f35
NU0RI <158851606+NU0RI@users.noreply.github.com>
2024-02-18 01:48:55 +0300

    Enemy Ai 1.0

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> patrolPonts;
+    public PlayerController player;
+    private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var direction = player.transform.position - transform.position;
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+        {
+            if(hit.collider.gameObject == player.gameObject)
+            {
+                _isPlayerNoticed = true;
+            }
+            else
+            {
+                _isPlayerNoticed = false;
+            }
+        }
+        else
+        {
+            _isPlayerNoticed = false;
+        }
+
+       if (_navMeshAgent.remainingDistance ==0 )
+        {
+            PickNewPatrolPoint();
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPonts[Random.Range(0, patrolPonts.Count)].position;
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+}
