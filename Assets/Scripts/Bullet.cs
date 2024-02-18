184cf4e75716fe60f394cd1b9c1195190d3f6e0a
NU0RI <158851606+NU0RI@users.noreply.github.com>
2024-02-18 22:41:54 +0300

    123

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * -Speed * Time.fixedDeltaTime;
+    }
+}
